9399bbdb8c8b38b9b8b46c9817bf092ab9e82620
Tanish Gupta <tanishgupta12389@gmail.com>
2025-10-26 11:27:20 +0530

    Analyzer api call

--- src/nutrition/analyzer.py
+++ src/nutrition/analyzer.py
@@ -1,1 +1,5 @@
 # Crunches daily eats: Calculates nutrition index (calories, macros, vitamins) using a lib like nutritionix or a simple DB.
+from fatsecret import Fatsecret
+
+
+fs = Fatsecret(cust_key,cust_secret)
